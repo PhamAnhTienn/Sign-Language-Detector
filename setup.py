694ed40d02f07e8db72210f01d76813cfa7058cc
Pham Anh Tien <anhtien13082005@gmail.com>
2024-09-12 18:14:22 +0700

    set up added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Sign Languages',
+    version= '0.0.0',
+    author= 'Tien',
+    author_email= 'tienpa.vn@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
