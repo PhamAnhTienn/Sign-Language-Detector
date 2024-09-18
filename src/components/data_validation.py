40bf3e02a93b4b7dbfca6622cc0b19dabc299503
Pham Anh Tien <anhtien13082005@gmail.com>
2024-09-18 15:23:02 +0700

    data validation added

---
+++ src/components/data_validation.py
@@ -0,0 +1,53 @@
+import os, sys
+import shutil
+from src.logger import logging
+from src.exception import SignException
+from src.entity.config_entity import DataValidationConfig
+from src.entity.artifacts_entity import DataIngestionArtifact, DataValidationArtifact
+
+class DataValidation:
+    def __init__(self, data_validation_config: DataValidationConfig, data_ingestion_artifact: DataIngestionArtifact):
+        try:
+            self.data_validation_config = data_validation_config
+            self.data_ingestion_artifact = data_ingestion_artifact
+        except Exception as e:
+            raise SignException(e, sys)
+        
+    def validate_all_files_exist(self) -> bool:
+        try:
+            validation_status = True
+            list_of_files = os.listdir(self.data_ingestion_artifact.feature_store_path)
+            
+            for file in list_of_files:
+                if file not in self.data_validation_config.required_file_list:
+                    validation_status = False
+                    os.makedirs(self.data_validation_config.data_validation_dir, exist_ok=True)
+                    with open(self.data_validation_config.validation_status_file, "w") as f:
+                        f.write(f"Validation status: {validation_status}")
+                    break
+
+                else:
+                    validation_status = True
+                    os.makedirs(self.data_validation_config.data_validation_dir, exist_ok=True)
+                    with open(self.data_validation_config.valid_status_file_dir, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+                
+        except Exception as e:
+            raise SignException(e, sys)
+    
+    def initiate_data_validation(self) -> DataValidationArtifact: 
+        logging.info("Entered initiate_data_validation method of DataValidation class")
+        try:
+            status = self.validate_all_files_exist()
+            data_validation_artifact = DataValidationArtifact(validation_status=status)
+            
+            logging.info("Exited initiate_data_validation method of DataValidation class")
+            logging.info(f"Data validation artifact: {data_validation_artifact}")
+            
+            if status:
+                shutil.copy(self.data_ingestion_artifact.data_zip_file_path, os.getcwd())
+            
+            return data_validation_artifact
+        
+        except Exception as e:
+            raise SignException(e, sys)
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -2,17 +2,17 @@ import sys, os
 from src.logger import logging
 from src.exception import SignException
 from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
-from src.entity.config_entity import DataIngestionConfig
-
-from src.entity.artifacts_entity import DataIngestionArtifact
+from src.entity.artifacts_entity import DataIngestionArtifact, DataValidationArtifact
 
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-    
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try: 
@@ -29,10 +29,27 @@ class TrainPipeline:
 
         except Exception as e:
             raise SignException(e, sys)
+    
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        try:
+            logging.info("Entered the start_data_validation method of TrainPipeline class")
+            
+            data_validation = DataValidation(data_validation_config = self.data_validation_config, data_ingestion_artifact = data_ingestion_artifact)
+            data_validation_artifact = data_validation.initiate_data_validation()
+            
+            logging.info("Performed the data validation operation")
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+            
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SignException(e, sys)
         
         
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
+            
         except Exception as e:
             raise SignException(e, sys)
